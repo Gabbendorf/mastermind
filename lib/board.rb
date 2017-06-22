3a5a689dfe758a3f27a6cb96513fb945789ea0cc
Gabriella <gabriellamds@yahoo.it>
2017-06-22 17:57:42 +0100

    board keeps track of all guesses

--- lib/board.rb
+++ lib/board.rb
@@ -5,10 +5,15 @@ class Board
   def initialize(rows)
     @rows = rows
     @colour_pattern = []
+    @guesses = []
   end
 
   def register_colour_pattern(colour)
     @colour_pattern.push(colour)
+  end
+
+  def register_guess(guess)
+    @guesses.push(guess)
   end
 
 end
