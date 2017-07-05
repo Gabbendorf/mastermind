484d85306517686c1d3c6680806f7c47ee9b24f4
Gabriella <gabriellamds@yahoo.it>
2017-07-05 14:03:37 +0100

    refactorings and polishing

--- lib/unbeatable_computer.rb
+++ lib/unbeatable_computer.rb
@@ -3,6 +3,7 @@ require_relative 'peg_colour'
 require_relative 'pattern'
 require_relative 'colour_list'
 
+
 class UnbeatableComputer
 
   attr_reader :possible_patterns
@@ -12,21 +13,21 @@ class UnbeatableComputer
     @board = board
     @possible_patterns = Set.new.to_a
     @colour_list = ColourList.new
-    @list_for_first_guess = ["green", "pink", "yellow", "purple", "blue", "orange"]
   end
+
+  LIST_FOR_FIRST_GUESS = ["green", "pink", "yellow", "purple", "blue", "orange"]
 
   def make_guess
     if @board.history.size == 0
-      random_first_guess
+      first_guess
     end
   end
 
   def generate_all_possible_patterns
     while !demonstrated_possible_patterns_generated?
-      random_pattern = []
-      @pattern_size.times {random_pattern.push(@colour_list.available_colours.sample)}
-      @possible_patterns.push(random_pattern)
-      random_pattern = []
+      random_colours = []
+      @pattern_size.times {random_colours.push(PegColour.new(@colour_list.available_colours.sample))}
+      @possible_patterns.push(Pattern.new(random_colours))
     end
   end
 
@@ -36,20 +37,19 @@ class UnbeatableComputer
     @possible_patterns.size == 1296
   end
 
-  def random_first_guess
-    two_colours = two_same_colours
-    first_guess = [two_colours[0], two_colours[0], two_colours[1], two_colours[1]]
+  def first_guess
+    two_colours = two_random_colours
+    first_colour = two_colours[0]
+    second_colour = two_colours[1]
+    first_guess = [first_colour, first_colour, second_colour, second_colour]
     Pattern.new(first_guess.map {|colour| PegColour.new(colour)})
   end
 
-  def two_same_colours
-    two_colours = []
-    first_colour = @list_for_first_guess.sample
-    two_colours.push(first_colour)
-    @list_for_first_guess.delete(first_colour)
-    second_colour = @list_for_first_guess.sample
-    two_colours.push(second_colour)
-    two_colours
+  def two_random_colours
+    first_colour = LIST_FOR_FIRST_GUESS.sample
+    LIST_FOR_FIRST_GUESS.delete(first_colour)
+    second_colour = LIST_FOR_FIRST_GUESS.sample
+    [first_colour, second_colour]
   end
 
 end
